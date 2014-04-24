7ad2d0cfac9acb217e177ee1aaf0d0f0765b9139
davejohn <johnson@mapbox.com>
2014-04-24 17:24:27 -0400

    updating site with unchanged_data.csv, and style updates

--- process.py
+++ process.py
@@ -6,16 +6,20 @@ from itertools import groupby
 #Process census file
 #********************************* 
 tracts = csv.DictReader(open('data/csv/2010_relationship.csv', 'rb'), delimiter = ',', quotechar = '"')
+tracts_sort = sorted(tracts, key = lambda x: x['GEOID10'])
+
+changed_tracts = csv.DictReader(open('data/csv/2010_substantial_changes.csv', 'rb'), delimiter = ',', quotechar = '"')
+changed_sort = sorted(changed_tracts, key = lambda x: x['GEOID10'])
 
 # Define functions
 final = {}
 final['2000'] = {}
 final['2010'] = {}
-
+changedIDs = []
 def parseData():
 	row_count = 0
 
-	for t in tracts:
+	for t in tracts_sort:
 		row_count +=1
 		changes = {}
 		changes['2000'] = {}
@@ -23,16 +27,16 @@ def parseData():
 		changes['2000']['part'] = t['PART10']
 		changes['2000']['poppct'] = t['POPPCT10']
 		changes['2000']['areapct'] = t['AREALANDPCT10PT']
-
+		
 		changes['2010'] = {}
 		changes['2010']['geoid'] = t['GEOID10']
 		changes['2010']['part'] = t['PART00']
 		changes['2010']['poppct'] = t['POPPCT00']
 		changes['2010']['areapct'] = t['ARELANDPCT00PT']
-
+		
 		tract1 = t['GEOID00']
 		tract2 = str(t['GEOID10'])
-
+		
 		try: 
 			final['2000'][tract1].append(changes['2010'])
 		except:
@@ -45,6 +49,10 @@ def parseData():
 			final['2010'][tract2] = []
 			final['2010'][tract2].append(changes['2000'])
 
+		for c in changed_sort:
+			if t['GEOID10'] == c['GEOID10']:
+				changedIDs.append(c['GEOID10'])
+
 	print "Total rows processed:", row_count
 	
 	writeout = json.dumps(final['2000'], sort_keys=True, separators=(',',':'))
@@ -54,6 +62,11 @@ def parseData():
 
 	writeout = json.dumps(final['2010'], sort_keys=True, separators=(',',':'))
 	f_out = open('data/json/2010.json', 'wb')
+	f_out.writelines(writeout)
+	f_out.close()
+
+	writeout = json.dumps(changedIDs, sort_keys=True, separators=(',',':'))
+	f_out = open('data/json/changed_2010.json', 'wb')
 	f_out.writelines(writeout)
 	f_out.close()
 
---
+++ script.js
@@ -0,0 +1,235 @@
+var map = L.mapbox.map('map', 'djohnson.map-4vq3wnwk', {minZoom: 6, maxZoom: 15})
+	.setView([41.85, -87.8], 10);
+
+var oldTractsGeo,
+	newTractsGeo,
+	oldTracts,
+	newTracts,
+	changedTracts,
+	closeTooltip,
+	colors = ['#FEA','#97B6C1','#8C9AC1','#E2FFD7','#8A9BCC','#B27667'],
+	layerGroup = L.layerGroup(),
+	newLayer,
+	popup = new L.Popup({ autoPan: false }),
+	layers = [],
+	sigChanges = [];
+// Get 2000 tract data
+$.ajax({
+	url: '/census-tracts/data/json/2000.json',
+	dataType: 'json',
+	success: function (json) {
+		oldTracts = json;
+	}
+});
+// Get 2010 tract data
+$.ajax({
+	url: '/census-tracts/data/json/2010.json',
+	dataType: 'json',
+	success: function (json) {
+		newTracts = json;
+		drawMap()
+	}
+});
+// Get 2000 tract geo data, ready to be called on click of 2010 tracts
+$.ajax({
+	url: '/census-tracts/data/geo/il_tracts_2000.json',
+	async: false,
+	dataType: 'json',
+	success: function (json) {
+		oldTractsGeo = json;
+	}
+});
+// 2000 tracts
+$.ajax({
+	url: '/census-tracts/data/json/changed_2010.json',
+	async: false,
+	dataType: 'json',
+		success: function (json) {
+		changedTracts = json;
+	}
+});
+// 2010 tracts
+$.ajax({
+	url: '/census-tracts/data/geo/il_tracts.json',
+	async: false,
+	dataType: 'json',
+	success: function (json) {
+		newTractsGeo = json;
+	}
+});
+
+function getStyle(feature) {
+	return {
+		weight: 2,
+		opacity: .3,
+		color: '#5A6981',
+		fillOpacity: 0.0,
+		fillColor: '#fff'
+	};
+}
+function clearLayers(array){
+	_.each(array, function(l, i) {
+		map.removeLayer(l);
+	});
+	array = []
+}
+function hashTracts(id) {
+	newData = newTracts[id];
+	var IDs = _.pluck(newData, 'geoid'),
+		selected,
+		features = topojson.feature(oldTractsGeo, oldTractsGeo.objects.tl_2010_17_tract00).features,
+		newFeatures = topojson.feature(newTractsGeo, newTractsGeo.objects.il_tracts).features;
+
+	_.each(IDs, function(iden, i) {
+		selected = _(features).findWhere({id: iden}),
+		newLayer = L.geoJson(selected,  {
+			style: {weight: 0,
+			opacity: 1,
+			color: colors[i],
+			fillOpacity: .7,
+			fillColor: colors[i]
+			} 
+		});
+		layerGroup.addLayer(newLayer, true);
+		// Push to layers array to clear later
+		layers.push(newLayer);
+		if (i == IDs.length - 1){map.addLayer(layerGroup,true)}
+	});
+}
+function showOldTracts(e) {
+	clearLayers(layers)
+	window.location.hash = '#'+e.target.feature.id;
+	// Uncomment below line to zoom to bounds on click
+	map.fitBounds(e.target.getBounds(),{paddingBottomRight:[320,50],paddingTopRight:[200,200]});
+	newData = newTracts[e.target.feature.id];
+	var IDs = _.pluck(newData, 'geoid'),
+		selected,
+		features = topojson.feature(oldTractsGeo, oldTractsGeo.objects.tl_2010_17_tract00).features;
+
+	_.each(IDs, function(iden, i) {
+		selected = _(features).findWhere({id: iden}),
+		newLayer = L.geoJson(selected,  {
+			style: {weight: 0,
+			opacity: 1,
+			color: colors[i],
+			fillOpacity: .7,
+			fillColor: colors[i]
+			} 
+		});
+		layerGroup.addLayer(newLayer, true);
+		// Push to layers array to clear later
+		layers.push(newLayer);
+		if (i == IDs.length - 1){map.addLayer(layerGroup,true)}
+	});
+}
+function onEachFeature(feature, layer) {
+	var hash = window.location.hash.replace(/#/, '');
+	if (feature.id == hash){
+		map.fitBounds(layer.getBounds(),{paddingBottomRight:[320,50],paddingTopRight:[200,200]});
+		hashTracts(feature.id)
+	}
+	layer.on({
+		mousemove: mousemove,
+		mouseout: mouseout,
+		click: showOldTracts
+	});
+}
+function mousemove(e) {
+	var layer = e.target,
+		rows,
+		newData = newTracts[layer.feature.id];
+
+	popup.setLatLng(e.latlng);
+	var parts = (_.pluck(newData, 'part')).join();
+	var rows = [],
+		table;
+
+	_.each(newData, function(x, i) {
+		rows.push('<tr style=background-color:'+colors[i]+';><td>'+ x.geoid +'</td><td>'+ x.part +'</td>'+
+		'<td>'+ x.poppct +'</td><td>'+ x.areapct +'</tr>')
+		if (i == newData.length - 1){
+		var formatRows = rows.join();
+		table = '<span class="large">2010 GEOID: ' + layer.feature.id + '</span><table><tr><td><em>This tract is composed of these <b>2000</b> tracts</em></td></tr><th>GEOID</th><th>Part</th><th>% Pop</th><th>% Area</th>' + rows + '</table>'
+		}
+	});
+	popup.setContent('<div class="title"></div>'+table);
+
+	if (!popup._map) popup.openOn(map);
+	window.clearTimeout(closeTooltip);
+	
+	// highlight feature
+	layer.setStyle({
+		weight: 4,
+		color: '#5A6981',
+		opacity: 0.8
+	});
+	
+	if (!L.Browser.ie && !L.Browser.opera) {
+		layer.bringToFront();
+		}
+}
+function mouseout(e) {
+	layer.resetStyle(e.target);
+	closeTooltip = window.setTimeout(function() {
+		map.closePopup();
+	}, 100);
+} 
+function drawAllTracts() {
+	clearLayers(sigChanges)
+	var topoFeatures = topojson.feature(newTractsGeo, newTractsGeo.objects.il_tracts).features;
+	layer = L.geoJson(topoFeatures, {
+	  style: getStyle,
+	  onEachFeature: onEachFeature
+	}).addTo(map);
+	sigChanges.push(layer)
+}
+function drawSigTracts() {
+	clearLayers(sigChanges)
+	var topoFeatures = topojson.feature(newTractsGeo, newTractsGeo.objects.il_tracts).features;
+	_.each(changedTracts, function(x, i) {
+		selectedFeature = _(topoFeatures).findWhere({id: x});
+		layer = L.geoJson(selectedFeature,  {
+			style: getStyle,
+			onEachFeature: onEachFeature
+		}).addTo(map);
+		sigChanges.push(layer)
+	});
+}
+
+function updateMap(type){
+	clearLayers(layers)
+	clearLayers(sigChanges)
+	if (type == 'sig'){
+		drawSigTracts();
+	} else {
+		drawAllTracts();
+	}
+}
+
+function drawMap(){
+	if (window.location.hash){
+		$('.switcher a').removeClass('active');
+		$('.switcher a.all').addClass('active');
+		drawAllTracts();
+	} else {
+		drawSigTracts();
+	}
+}
+
+// Bring up the about panel
+$('.switcher a').click(function() {
+	window.location.hash = '';
+	map.setView([40.4, -88], 7);
+	$('.switcher a').removeClass('active');
+	$(this).addClass('active');
+	updateMap($(this).attr('type'))
+});
+
+// Bring up the about panel
+$('.about').click(function() {
+	$('#about').toggleClass('active');
+});
+// Reposition map on IL icon click
+$('img.il').click(function() {
+	map.setView([40.4, -88], 7);
+});
